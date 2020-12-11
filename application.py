5e5e49abaf52e5260b4407ba663b8af61594575a
Simon Ricard <ricardsimon@hotmail.com>
2020-12-10 19:54:50 -0500

    Another fix

--- application.py
+++ application.py
@@ -286,4 +286,4 @@ for code in default_exceptions:
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
